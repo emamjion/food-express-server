0dcdaaedfb83194235d19edb8fe1ccc1e17077bb
emamjion <emamjion@gmail.com>
2023-05-04 05:13:46 +0600

    json file created and updated

--- index.js
+++ index.js
@@ -1,10 +1,18 @@
 const express = require('express');
+const cors = require('cors');
 const app = express();
 const port = process.env.port || 5000;
 
+const chefs = require('./data/chefs.json');
+
+app.use(cors());
 app.get('/', (req, res) => {
     res.send('food express running')
 });
+
+app.get('/chefs', (req, res) => {
+    res.send(chefs);
+})
 
 
 app.listen(port, () => {
