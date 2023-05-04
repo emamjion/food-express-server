455fe25898e97a85d72e06fada7b152a90182195
emamjion <emamjion@gmail.com>
2023-05-04 06:52:25 +0600

    server side data almost ready

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const app = express();
 const port = process.env.port || 5000;
 
 const chefs = require('./data/chefs.json');
+const chefDetails = require('./data/chefDetails.json');
 
 app.use(cors());
 app.get('/', (req, res) => {
@@ -12,6 +13,25 @@ app.get('/', (req, res) => {
 
 app.get('/chefs', (req, res) => {
     res.send(chefs);
+});
+
+app.get('/chefDetails', (req, res) => {
+    res.send(chefDetails);
+});
+
+
+app.get('/chefDetails/:id', (req, res) => {
+    const id = req.params.id;
+    // console.log(id);
+    const selectedChef = chefDetails.find(n => n.id === id);
+    res.send(selectedChef);
+});
+
+app.get('/chefs/:id', (req, res) => {
+    const id = req.params.id;
+    // console.log(id);
+    const chefAndDetails = chefDetails.filter(n => n.id == id);
+    res.send(chefAndDetails);
 })
 
 
